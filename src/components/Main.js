2d8622f84b250ad681874976935bd90b796a42b0
BereketGirma <bereketgirma002@gmail.com>
2024-06-03 21:52:22 -0500

    Update main
    Job description, certifications and contact have now been fully designed.

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,84 @@ import './Main.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faGithub , faLinkedinIn} from '@fortawesome/free-brands-svg-icons';
 
+const jobDetail = {
+    'ITSolutions' : {
+        title: 'IT Student Consultant',
+        time: 'August 2023 - Present',
+        tasks:[
+            'Provided personalized IT solutions to students, faculty, and staff, addressing hardware and software issues.',
+            'Collaborated with cross-functional teams to troubleshoot and resolve technical problems promptly.'
+            ]
+        },
+    'MavPASS-CIS' : {
+        title: 'IT Student Consultant',
+        time: 'August 2023 - Present',
+        tasks:[
+            'Provided personalized IT solutions to students, faculty, and staff, addressing hardware and software issues.'
+            ]
+        },
+    'MavPASS-PHY' : {
+        title: 'IT Student Consultant',
+        time: 'August 2023 - Present',
+        tasks:[
+            'Provided personalized IT solutions to students, faculty, and staff, addressing hardware and software issues.',
+            'Collaborated with cross-functional teams to troubleshoot and resolve technical problems promptly.',
+            'Collaborated with cross-functional teams to troubleshoot and resolve technical problems promptly.'
+            ]
+        }
+}
+
+function showJobCard(event){
+    const buttons = document.querySelectorAll('.job')
+    buttons.forEach(button => {
+        button.setAttribute('aria-selected','false')
+    })
+    event.currentTarget.setAttribute('aria-selected','true')
+
+    const jobCard = document.getElementById('job-card');
+    if(jobCard){
+    const job = event.currentTarget.getAttribute('data-job');
+    const job_info = jobDetail[job]
+
+        if(job_info){
+            jobCard.querySelector('.job-title').textContent = job_info.title
+            jobCard.querySelector('.job-time').textContent = job_info.time
+
+            const existingContainer = jobCard.querySelectorAll('.arrow-text-container');
+            existingContainer.forEach(container => container.remove());
+
+            job_info.tasks.forEach(task => {
+                const container = document.createElement('div');
+                container.className = 'arrow-text-container';
+
+                const arrow = document.createElement('div');
+                arrow.className = 'arrow'
+
+                const taskElement = document.createElement('p');
+                taskElement.className = 'job-task'
+                taskElement.textContent = task;
+
+                container.appendChild(arrow);
+                container.appendChild(taskElement);
+                jobCard.appendChild(container);
+            });
+       } else {
+        console.error('Job information not found for:',job);
+       }
+    
+    } else {
+        console.error('Job information not found');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    window.onload = function() {
+        document.querySelector('.job[data-job="ITSolutions"]').click();
+    };
+});
+
+
+
 const About = ()=>{
     return(
         <div className="page">
@@ -124,6 +202,48 @@ const About = ()=>{
                         <div className="box item-1">Hello</div>
                         <div className="box item-2">Hello</div>
                         <div className="box item-3">Hello</div>
+                    </div>
+                </div>
+
+                <div className="experience-container">
+                    <div className="experience">
+                        <div className="title"><h1>Experience</h1></div>
+                        <div className="title-line"></div>
+                    </div>
+                    <div className="jobs-container">
+                        <div className="button-tile" role="tablist">
+                            <button className="job" role="tab" aria-selected = "true" data-job = "ITSolutions" onClick={(e) => showJobCard(e)}>IT Solutions</button>
+                            <button className="job" role="tab" aria-selected = "false" data-job = "MavPASS-CIS" onClick={(e) => showJobCard(e)}>MavPASS  CIS</button>
+                            <button className="job" role="tab" aria-selected = "false" data-job = "MavPASS-PHY" onClick={(e) => showJobCard(e)}>MavPASS  PHY</button>
+                        </div>
+                        <div className="job-card" id = "job-card">
+                                <h3 className="job-title"></h3>
+                                <p className="job-time"></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="certificate-container">
+                    <h1 className="certificate-title">Certificates</h1>
+                    <div className="certificate-box">
+                        <img src = "" alt = "Certificate-one-Image" className="box certificate-1"/>
+                        <img src = "" alt = "Certificate-one-Image" className="box certificate-1"/>
+                        <img src = "" alt = "Certificate-one-Image" className="box certificate-1"/>
+                    </div>
+                </div>
+
+                <div className="contact-container">
+                    <div className="contact">
+                        <div className="title"><h1>Contact Me</h1></div>
+                        <div className="title-line"></div>
+                    </div>
+
+                    <div className="contact-content">
+                        <div className="contact-description">
+                            <p>I am actively seeking new opportunities in software engineering. With a strong background in software development, problem-solving, and team collaboration, I am eager to contribute my skills to a dynamic team. Feel free to reach out to me at bereketgirma002@gmail.com </p>
+                        </div>
+
+                        <button className="contact-button">Say Hello!</button>
                     </div>
                 </div>
             </div>
