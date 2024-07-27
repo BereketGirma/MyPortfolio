8de277ad69a2933cbba5a8e862e8b4c627361000
BereketGirma <bereketgirma002@gmail.com>
2024-07-27 13:35:59 -0500

    Update
    Certificate and project mobile version finished

--- src/components/Certificate.js
+++ src/components/Certificate.js
@@ -1,5 +1,16 @@
 import './Certificate.css';
+import {React,useState,useEffect} from 'react';
+
 const Certificate = () => {
+
+    const [activeTab, setActiveTab] = useState('');
+    const handleTabClick = (tabId) => {
+        setActiveTab(tabId)
+    }
+
+    useEffect(() => {
+        setActiveTab('test1');
+    },[]);
     return(
         <div className="certificate-container">
             <h1 className="certificate-title">Certificates</h1>
@@ -26,6 +37,57 @@ const Certificate = () => {
                         src = {require("./Images/Certificate Images/CodePath-AND101-Certificate.jpg")} 
                         alt = "Not Found"
                     />
+                </div>
+
+                {/* mobile version for tabs */}
+                <div id = "mobile" className='tabs'>
+                    <button 
+                        className={`tabLinks ${activeTab === 'test1' ? 'active':''}`}
+                        onClick={() => handleTabClick('test1')}>
+                        CodePath
+                    </button>
+                    <button 
+                        className={`tabLinks ${activeTab === 'test2' ? 'active':''}`} 
+                        onClick={() => handleTabClick('test2')}>
+                        MavPASS-1
+                    </button>
+                    <button 
+                        className={`tabLinks ${activeTab === 'test3' ? 'active':''}`} 
+                        onClick={() => handleTabClick('test3')}>
+                        MavPASS-2
+                    </button>
+                </div>
+
+                <div className='tabcontent'>
+                    {activeTab === 'test1' && (
+                        <div className='image'>
+                            <h3>CodePath AND-101</h3>
+                            <img 
+                                src = {require("./Images/Certificate Images/CodePath-AND101-Certificate.jpg")} 
+                                alt = "Not found" 
+                            /> 
+                        </div>
+                    )}
+
+                    {activeTab === 'test2' && (
+                        <div className='image'>
+                            <h3>MavPASS CIS-121</h3>
+                            <img 
+                                src = {require("./Images/Certificate Images/MavPASS-CIS121.jpg")} 
+                                alt = "Not found" 
+                            />
+                        </div>
+                    )}
+
+                    {activeTab === 'test3' && (
+                        <div className='image'>
+                            <h3>MavPASS PHYS-211</h3>
+                            <img 
+                                src = {require("./Images/Certificate Images/MavPASS-PHYS211.jpg")} 
+                                alt = "Not found" 
+                            />
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
