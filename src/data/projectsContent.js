05ef7067ed26bb74d6a376b9a97ffb4b8cfbd294
BereketGirma <bereketgirma002@gmail.com>
2024-10-27 10:19:31 -0500

    Renamed content files

---
+++ src/data/projectsContent.js
@@ -0,0 +1,28 @@
+export const projectContent = {
+    project1 : {
+        title: "Lob-Bot",
+        description: `LobBot is a vertisile Telegram bot developed using Telegram API.
+            It enhances your Telegram experience by offering features such as searching
+            for information, managing messages,sharing user status and more. Perfect for 
+            both practical tasks and enjoyment, LOB-BOT makes interacting on Telegram more 
+            efficient and enjoyable.`,
+        softwares: "Telegram-API | Python |",
+        github: "https://github.com/BereketGirma/LobBot"
+    },
+
+    project2: {
+        title: "Maverick Maps",
+        description: `Maverick Maps is a tool that is built for students, faculty, 
+            staff and vistors to have an easier time navigating through the campus of 
+            Minnesota State University,Mankato. It is an app built with React-Native 
+            along side Unity which displays a 3D Map of the campus layout. It is an
+            easy access tool with user friendly UI design.`,
+        softwares: "TypeScript | React-Native | Unity",
+        github: ""
+    },
+
+    project3: {
+        title: "Shift Exporter",
+        description: ``
+    }
+}
