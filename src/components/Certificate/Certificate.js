31713d288f2d2d4f044ff83fb2d41a141b58ff16
BereketGirma <bereketgirma002@gmail.com>
2024-10-27 20:05:21 -0500

    Update to certificate section
    Made a slide show for images
    Added buttons to switch between images for easier access

--- src/components/Certificate/Certificate.js
+++ src/components/Certificate/Certificate.js
@@ -1,93 +1,45 @@
 import './Certificate.css';
 import {React,useState,useEffect} from 'react';
+import { certificateContent } from '../../data/certificateContent';
 
 const Certificate = () => {
 
-    const [activeTab, setActiveTab] = useState('');
-    const handleTabClick = (tabId) => {
-        setActiveTab(tabId)
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const handleNext = () => {
+        setActiveIndex((prevIndex) => (prevIndex + 1 + certificateContent.length) % certificateContent.length)
+    }
+
+    const handlePrevious = () => {
+        setActiveIndex((prevIndex) => (prevIndex - 1 + certificateContent.length) % certificateContent.length)
     }
 
     useEffect(() => {
-        setActiveTab('test1');
-    },[]);
+        const interval = setInterval(handleNext, 3000)
+        return () => clearInterval(interval)
+    })
+
     return(
         <div className="certificate-container">
             <h1 className="certificate-title">Certificates</h1>
             <div className="certificate-box">
-                <div className="certificate one">
-                    <p>MavPASS PHYS-211</p>
-                    <img 
-                        src = {require("../../assets/Images/Certificate Images/MavPASS-PHYS211.jpg")} 
-                        alt = "Not found" 
-                    />
+                <div className="certificate-info">
+                    <h3>{certificateContent[activeIndex].title}</h3>
                 </div>
                 
-                <div className="certificate two">
-                    <p>MavPASS CIS-121</p>
-                    <img 
-                        src = {require("../../assets/Images/Certificate Images/MavPASS-CIS121.jpg")} 
-                        alt = "Not Found"
+                {certificateContent.map((cert, index) => (
+                    <img
+                        key={index}
+                        src={cert.imgSrc}
+                        alt={cert.title}
+                        className={`certificate-image ${activeIndex === index ? 'active' : ''}`}
                     />
-                </div>
-                        
-                <div className="certificate three">
-                    <p>CodePath AND-101</p>
-                    <img 
-                        src = {require("../../assets/Images/Certificate Images/CodePath-AND101-Certificate.jpg")} 
-                        alt = "Not Found"
-                    />
-                </div>
+                ))}
+                
 
-                {/* mobile version for tabs */}
-                <div id = "mobile" className='tabs'>
-                    <button 
-                        className={`tabLinks ${activeTab === 'test1' ? 'active':''}`}
-                        onClick={() => handleTabClick('test1')}>
-                        CodePath
-                    </button>
-                    <button 
-                        className={`tabLinks ${activeTab === 'test2' ? 'active':''}`} 
-                        onClick={() => handleTabClick('test2')}>
-                        MavPASS-1
-                    </button>
-                    <button 
-                        className={`tabLinks ${activeTab === 'test3' ? 'active':''}`} 
-                        onClick={() => handleTabClick('test3')}>
-                        MavPASS-2
-                    </button>
-                </div>
-
-                <div className='tabcontent'>
-                    {activeTab === 'test1' && (
-                        <div className='image'>
-                            <h3>CodePath AND-101</h3>
-                            <img 
-                                src = {require("../../assets/Images/Certificate Images/CodePath-AND101-Certificate.jpg")} 
-                                alt = "Not found" 
-                            /> 
-                        </div>
-                    )}
-
-                    {activeTab === 'test2' && (
-                        <div className='image'>
-                            <h3>MavPASS CIS-121</h3>
-                            <img 
-                                src = {require("../../assets/Images/Certificate Images/MavPASS-CIS121.jpg")} 
-                                alt = "Not found" 
-                            />
-                        </div>
-                    )}
-
-                    {activeTab === 'test3' && (
-                        <div className='image'>
-                            <h3>MavPASS PHYS-211</h3>
-                            <img 
-                                src = {require("../../assets/Images/Certificate Images/MavPASS-PHYS211.jpg")} 
-                                alt = "Not found" 
-                            />
-                        </div>
-                    )}
+                <div className='slideshow-navigation'>
+                    <button onClick={handlePrevious}>&lt; Previous</button>
+                    <button onClick={handleNext}>Next &gt;</button>
                 </div>
             </div>
         </div>
