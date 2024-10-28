267bb479e8d9cd48686fb2b7fd03abb517fd5966
BereketGirma <bereketgirma002@gmail.com>
2024-10-27 20:05:34 -0500

    Added certificate content

---
+++ src/data/certificateContent.js
@@ -0,0 +1,18 @@
+export const certificateContent = [
+
+    {
+        title: 'CodePath AND-101',
+        imgSrc: require("../assets/Images/Certificate Images/CodePath-AND101-Certificate.jpg")
+    },
+
+    {
+        title: 'MavPASS CIS-121',
+        imgSrc: require("../assets/Images/Certificate Images/MavPASS-CIS121.jpg")
+    },
+    
+    {
+        title: 'MavPASS PHY-211',
+        imgSrc: require("../assets/Images/Certificate Images/MavPASS-PHYS211.jpg")
+    }
+]
+
