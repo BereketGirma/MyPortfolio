944c6687645d5e0d964d87b74ff0f8cfa18baa45
BereketGirma <bereketgirma002@gmail.com>
2024-11-03 00:31:14 -0500

    updated contact section
    - added responsive elements
    - added contact contents

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,23 +1,28 @@
 import './Contact.css'
+import { contactContent } from '../../data/contactContent'; 
 const Contact = () => {
-    const handleClick = (event) => {
+    const handleEmailClick = (event) => {
         event.preventDefault();
         window.location.href = 'mailto:bereketgirma002@gmail.com';
     };
     
     return(
-        <div className="contact-container" id ="contact">
+        <div className="contact-container">
             <div className="contact">
-                <div className="title"><h1>Contact Me</h1></div>
-                <div className="title-line"></div>
+                <div className="contact-title"><h1>Contact Me</h1></div>
+                <div className="contact-title-line"></div>
             </div>
 
             <div className="contact-content">
                 <div className="contact-description">
-                    <p>I am actively seeking new internship opportunities in software engineering. With a strong background in software development, problem-solving, and team collaboration, I am eager to contribute my skills to a dynamic team. Feel free to reach out to me at bereketgirma002@gmail.com </p>
+                    <p>
+                        {contactContent.desciption}
+                        <span><u onClick={handleEmailClick}>{contactContent.email}</u></span>
+                    </p>
+
                 </div>
 
-                <button className="contact-button" onClick={handleClick}>Say Hello!</button>
+                <button className="contact-button" onClick={handleEmailClick}>Say Hello!</button>
             </div>
         </div>
     )
---
+++ src/data/contactContent.js
@@ -0,0 +1,7 @@
+export const contactContent = {
+    desciption : `I am actively seeking new internship opportunities 
+            in software engineering. With a strong background in software development,
+             problem-solving, and team collaboration, I am eager to contribute my 
+             skills to a dynamic team. Feel free to reach out to me at `,
+    email: `bereketgirma002@gmail.com`
+}
