b4a5dd192c942f64d0db970e622c5b1c6d9ff89b
BereketGirma <bereketgirma002@gmail.com>
2025-02-08 23:35:09 -0600

    Adding experience details

--- src/app/data/Experience.ts
+++ src/app/data/Experience.ts
@@ -4,36 +4,35 @@ export const experienceContent = {
         title: 'IT Lead Consultant',
         time: 'August 2023 - Present',
         tasks:[
-            `Provided personalized IT solutions to students, faculty, and 
-            staff, addressing hardware and software issues.`,
+            `As an IT Solutions Lead Consultant, I supervised a team of 30+ members, ensuring high performance and seamless collaboration in a fast-paced technical environment.`,
 
-            `Collaborated with cross-functional teams to troubleshoot and 
-            resolve technical problems promptly.`
+            `I provided hardware and software support to over 200 users, proactively troubleshooting issues and implementing solutions that reduced downtime by 15%.`,
+
+            `Leveraging TeamViewer and other remote support tools, I efficiently diagnosed and resolved technical issues, leading to a 30% reduction in support ticket resolution time.`,
+
+            `Collaborated with cross-functional teams to troubleshoot and resolve technical problems promptly.`
             ]
         },
 
     job2 : {
         button: 'MavPASS-CIS',
-        title: 'MavPass Leader for CIS 121(Introduction to programming)',
+        title: 'MavPass Leader for CIS 121(Intro to programming)',
         time: 'January 2024 - May 2024',
         tasks:[
-            `Provided personalized IT solutions to students, faculty, and staff,
-             addressing hardware and software issues.`
+            `Supported over 100 students in academic sessions focusing on programming concepts and course material`,
+            `Developed targeted support by attending lectures and identifying common student challenges`,
+            `Promoted collaborative learning through group problem solving activities`,
             ]
         },
     job3 : {
         button: 'MavPASS-PHY',
-        title: 'IT Student Consultant',
+        title: 'MavPass Leader for PHY211(General physics)',
         time: 'August 2023 - December 2024',
         tasks:[
-            `Provided personalized IT solutions to students, faculty, and 
-            staff, addressing hardware and software issues.`,
-
-            `Collaborated with cross-functional teams to troubleshoot and 
-            resolve technical problems promptly.`,
-
-            `Collaborated with cross-functional teams to troubleshoot and 
-            resolve technical problems promptly.`
+            `Supported over 40 students in academic sessions focusing on physics concepts and course material`,
+            `Attended course I was supporting and planned potential questions to work on with students`,
+            `Promoted collaborative learning through group problem solving activities`,
+            `Held office hours to provide additional help for students`,
             ]
         }
 }
