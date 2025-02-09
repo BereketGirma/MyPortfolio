0981f683ec2f8d39cefa222d539d4ac37cb00ab8
BereketGirma <bereketgirma002@gmail.com>
2025-02-09 00:01:08 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  assetPrefix: '/MyPortfolio/',
+  images: { unoptimized: true },
+  reactStrictMode: true
 };
 
 export default nextConfig;
